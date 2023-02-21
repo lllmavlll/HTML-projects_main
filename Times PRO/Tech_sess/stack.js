9b1dba9dad2b05bfadff37dd836c4d51a77c0a5e
Mahesh <maheshdsu86@gmail.com>
2023-02-21 13:31:02 +0530

    fvf

--- Times PRO/Tech_sess/stack.js
+++ Times PRO/Tech_sess/stack.js
@@ -9,3 +9,6 @@ console.log("top most element is: " + stack);
 var deleted = stack.pop();
 top = top - 1;
 console.log("Stack after pop: " + stack);
+
+//jquery, data structure( linear and non-linear)
+//
